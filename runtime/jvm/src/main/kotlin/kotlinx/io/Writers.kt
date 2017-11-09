ca620917313d4342646640014d16dd58404f05c8
Leonid Startsev <sandwwraith@gmail.com>
2017-11-09 19:55:51 +0300

    1.2 bootstrap preparation

--- runtime/jvm/src/main/kotlin/kotlinx/io/Writers.kt
+++ runtime/jvm/src/main/kotlin/kotlinx/io/Writers.kt
@@ -16,12 +16,12 @@
 
 package kotlinx.io
 
-@Suppress("HEADER_CLASS_MEMBERS_ARE_NOT_IMPLEMENTED")
+@Suppress("NO_ACTUAL_CLASS_MEMBER_FOR_EXPECTED_CLASS")
 impl typealias Writer = java.io.Writer
 
 impl typealias PrintWriter = java.io.PrintWriter
 impl typealias StringWriter = java.io.StringWriter
 
-@Suppress("HEADER_CLASS_MEMBERS_ARE_NOT_IMPLEMENTED")
+@Suppress("NO_ACTUAL_CLASS_MEMBER_FOR_EXPECTED_CLASS")
 impl typealias Reader = java.io.Reader
 impl typealias StringReader = java.io.StringReader
