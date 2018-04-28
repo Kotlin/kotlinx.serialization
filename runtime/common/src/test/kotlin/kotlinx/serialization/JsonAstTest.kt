a95d475bbe89b2d7e1b2dcefb6edc4c6be121191
Leonid Startsev <sandwwraith@gmail.com>
2018-04-28 15:05:33 +0300

    [JSON-AST] add convenient methods and tests

--- runtime/common/src/test/kotlin/kotlinx/serialization/JsonAstTest.kt
+++ runtime/common/src/test/kotlin/kotlinx/serialization/JsonAstTest.kt
@@ -8,7 +8,6 @@ class JsonAstTest {
     fun parseWithoutExceptions() {
         val input = """{"a": "foo",              "b": 10, "c": ["foo", 100500, {"bar": "baz"}]}"""
         val elem = JsonAstReader(input).readFully()
-        println(elem)
     }
 
     @Test
@@ -19,13 +18,35 @@ class JsonAstTest {
 
     @Test
     fun jsonObject() {
-        val inp = """{"a": "foo",              "b": 10}"""
-        val elem = JsonAstReader(inp).readFully()
+        val input = """{"a": "foo","b": 10, "c": true, "d": null}"""
+        val elem = JsonAstReader(input).readFully()
+
         assertTrue(elem is JsonObject)
         elem as JsonObject
-        assertEquals(setOf("a", "b"), elem.keys)
+        assertEquals(setOf("a", "b", "c", "d"), elem.keys)
+
         assertEquals(JsonValue("foo"), elem["a"])
-        val ten = elem.getValue("b") as JsonValue
-        assertEquals(10, ten.asInt)
+        assertEquals(10, elem.getAsValue("b")?.asInt)
+        assertEquals(true, elem.getAsValue("c")?.asBoolean)
+        assertTrue(elem.getValue("d") === JsonNull)
+    }
+
+    @Test
+    fun jsonObjectWithArrays() {
+        val input = """{"a": "foo",              "b": 10, "c": ["foo", 100500, {"bar": "baz"}]}"""
+        val elem = JsonAstReader(input).readFully()
+
+        assertTrue(elem is JsonObject)
+        elem as JsonObject
+        assertEquals(setOf("a", "b", "c"), elem.keys)
+        assertTrue(elem.getValue("c") is JsonArray)
+
+        val array = elem.getAsArray("c")!!
+        assertEquals("foo", array.getAsValue(0)?.str)
+        assertEquals(100500, array.getAsValue(1)?.asInt)
+
+        assertTrue(array[2] is JsonObject)
+        val third = array.getAsObject(2)!!
+        assertEquals("baz", third.getAsValue("bar")?.str)
     }
 }
