c563d23b6ff7303838bb57749735af4b8f901085
Leonid Startsev <sandwwraith@gmail.com>
2018-05-07 20:29:23 +0300

    [JSON-AST] Publishing setup

---
+++ example-ios/ios-test-application/ViewController.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+
+- (IBAction)buttonPressed;
+
+@end
