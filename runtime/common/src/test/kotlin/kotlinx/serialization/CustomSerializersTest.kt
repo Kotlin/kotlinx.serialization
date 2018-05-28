4dcefef55b5a1c3cfcd76203edd011cd0c3c6908
Leonid Startsev <sandwwraith@gmail.com>
2018-05-28 14:41:49 +0300

    Add test for quoted JSON for one of the optional cases Update copyright

--- runtime/common/src/test/kotlin/kotlinx/serialization/CustomSerializersTest.kt
+++ runtime/common/src/test/kotlin/kotlinx/serialization/CustomSerializersTest.kt
@@ -1,17 +1,17 @@
 /*
- *  Copyright 2017 JetBrains s.r.o.
+ * Copyright 2018 JetBrains s.r.o.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package kotlinx.serialization
@@ -226,6 +226,13 @@ class CustomSerializersTest {
         val obj = CList1(listOf(C(a = 1), C(b = 2), C(3, 4)))
         val s = JSON(unquoted = true).stringify(obj)
         assertEquals("{c:[{b:42,a:1},{b:2},{b:4,a:3}]}", s)
+    }
+
+    @Test
+    fun writeOptionalList1Quoted() {
+        val obj = CList1(listOf(C(a = 1), C(b = 2), C(3, 4)))
+        val s = JSON(unquoted = false).stringify(obj)
+        assertEquals("""{"c":[{"b":42,"a":1},{"b":2},{"b":4,"a":3}]}""", s)
     }
 
     @Test
