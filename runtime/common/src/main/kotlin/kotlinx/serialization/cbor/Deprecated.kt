6eb4042f9c780c7a3ca0b1d40e159b7d0a94d112
BenWoodworth <ben@benwoodworth.net>
2018-11-06 10:32:32 -0500

    Removed type aliases for Cbor.CborEncoder and Cbor.CborDecoder

--- runtime/common/src/main/kotlin/kotlinx/serialization/cbor/Deprecated.kt
+++ runtime/common/src/main/kotlin/kotlinx/serialization/cbor/Deprecated.kt
@@ -7,10 +7,3 @@ typealias CBOR = Cbor
 
 @Deprecated(deprecationText, ReplaceWith("CborDecodingException"), DeprecationLevel.WARNING)
 typealias CBORDecodingException = CborDecodingException
-
-// TODO Nested and local type aliases are not supported
-//@Deprecated(deprecationText, ReplaceWith("CborEncoder"), DeprecationLevel.WARNING)
-//typealias Cbor.CBOREncoder = Cbor.CborEncoder
-//
-//@Deprecated(deprecationText, ReplaceWith("CborDecoder"), DeprecationLevel.WARNING)
-//typealias Cbor.CBORDecoder = Cbor.CborDecoder
