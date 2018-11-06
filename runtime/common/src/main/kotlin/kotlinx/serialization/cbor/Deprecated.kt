733567baa2202a9d128d3e6e0165b6a364de4dc6
BenWoodworth <ben@benwoodworth.net>
2018-11-06 10:31:15 -0500

    Fixed CBOR type alias.

--- runtime/common/src/main/kotlin/kotlinx/serialization/cbor/Deprecated.kt
+++ runtime/common/src/main/kotlin/kotlinx/serialization/cbor/Deprecated.kt
@@ -3,7 +3,7 @@ package kotlinx.serialization.cbor
 import kotlinx.serialization.deprecationText
 
 @Deprecated(deprecationText, ReplaceWith("Cbor"), DeprecationLevel.WARNING)
-typealias CBOR = CborDecodingException
+typealias CBOR = Cbor
 
 @Deprecated(deprecationText, ReplaceWith("CborDecodingException"), DeprecationLevel.WARNING)
 typealias CBORDecodingException = CborDecodingException
