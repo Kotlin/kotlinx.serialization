1cc1dcfa2d628d24bb9dc3f99719d1162c7e4930
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2019-02-28 00:56:32 +0100

    Update App.kt
    order jsonData to right place

--- example-native/src/App.kt
+++ example-native/src/App.kt
@@ -7,9 +7,10 @@ data class Data(val a: Int, @Optional val b: String = "42")
 fun main(args: Array<String>) {
     // serializing objects
     val jsonData = Json.stringify(Data.serializer(), Data(42))
+    println(jsonData) // {"a": 42, "b": "42"}
+    
     // serializing lists
     val jsonList = Json.stringify(Data.serializer().list, listOf(Data(42)))
-    println(jsonData) // {"a": 42, "b": "42"}
     println(jsonList) // [{"a": 42, "b": "42"}]
 
     // parsing data back
