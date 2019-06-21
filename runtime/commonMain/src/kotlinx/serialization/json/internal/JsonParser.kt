89c9cdeab12f88ef7048aa8d081a7e25be75d21b
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2019-06-21 15:29:44 +0300

    Robust JsonArray parsing
      * Prohibit trailing, leading and consecutive commas
      * Require commas between array elements
      * Slightly optimize StreamingJsonInput.decodeElementIndex

--- runtime/commonMain/src/kotlinx/serialization/json/internal/JsonParser.kt
+++ runtime/commonMain/src/kotlinx/serialization/json/internal/JsonParser.kt
@@ -11,15 +11,15 @@ internal class JsonParser(private val reader: JsonReader) {
     private fun readObject(): JsonElement {
         reader.requireTokenClass(TC_BEGIN_OBJ) { "Expected start of object" }
         reader.nextToken()
-        val result: MutableMap<String, JsonElement> = linkedMapOf()
+        val result = linkedMapOf<String, JsonElement>()
         while (true) {
             if (reader.tokenClass == TC_COMMA) reader.nextJsonKey()
             if (!reader.canBeginValue) break
             val key = reader.takeString()
             reader.requireTokenClass(TC_COLON) { "Expected ':'" }
             reader.nextToken()
-            val elem = read()
-            result[key] = elem
+            val element = read()
+            result[key] = element
         }
         reader.requireTokenClass(TC_END_OBJ) { "Expected end of object" }
         reader.nextToken()
@@ -32,16 +32,26 @@ internal class JsonParser(private val reader: JsonReader) {
     }
 
     private fun readArray(): JsonElement {
-        reader.requireTokenClass(TC_BEGIN_LIST) { "Expected start of array" }
+        reader.requireTokenClass(TC_BEGIN_LIST) { "Expected start of the array" }
         reader.nextToken()
-        val result: MutableList<JsonElement> = arrayListOf()
-        while (true) {
-            if (reader.tokenClass == TC_COMMA) reader.nextToken()
-            if (!reader.canBeginValue) break
-            val elem = read()
-            result.add(elem)
+        // Prohibit leading comma
+        reader.require(reader.tokenClass != TC_COMMA, reader.currentPosition) { "Unexpected leading comma" }
+        val result = arrayListOf<JsonElement>()
+        var valueExpected = false
+        while (reader.canBeginValue) {
+            valueExpected = false
+            val element = read()
+            result.add(element)
+            if (reader.tokenClass != TC_COMMA) {
+                // Prohibit whitespaces instead of commas [a b]
+                reader.requireTokenClass(TC_END_LIST) { "Expected end of the array or comma" }
+            } else {
+                valueExpected = true
+                reader.nextToken()
+            }
         }
-        reader.requireTokenClass(TC_END_LIST) { "Expected end of array" }
+        // Prohibit trailing commas
+        reader.require(!valueExpected, reader.currentPosition) { "Expected end of the array or comma" }
         reader.nextToken()
         return JsonArray(result)
     }
