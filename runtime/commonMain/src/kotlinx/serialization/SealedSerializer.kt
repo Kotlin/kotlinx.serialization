9fea204e4682dae4a7fa55736202b4f8578f69f9
Leonid Startsev <sandwwraith@gmail.com>
2020-01-17 18:50:25 +0300

    Optimize space allocation for SerialClassDescImpl
    by passing actual serializable properties count in the generated code (and sometimes for default runtime serializers)

--- runtime/commonMain/src/kotlinx/serialization/SealedSerializer.kt
+++ runtime/commonMain/src/kotlinx/serialization/SealedSerializer.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2017-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package kotlinx.serialization
@@ -105,7 +105,7 @@ public class SealedClassSerializer<T : Any>(
 internal class SealedClassDescriptor(
     name: String,
     elementDescriptors: List<SerialDescriptor>
-) : SerialClassDescImpl(name) {
+) : SerialClassDescImpl(name, elementsCount = elementDescriptors.size) {
     override val kind: SerialKind = PolymorphicKind.SEALED
 
     init {
