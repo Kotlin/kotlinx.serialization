9cc7fb2cc4ee50b9c7bd0fb735907d5069bdcec5
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2020-01-22 13:25:09 +0300

    Detect collisions with class discriminator and for equal serial names within the same sealed class hierarchy
    Fixes #457

---
+++ runtime/commonTest/src/kotlinx/serialization/modules/SerialNameCollisionInSealedClassesTest.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2017-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package kotlinx.serialization.modules
+
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+import kotlin.test.*
+
+private const val prefix = "kotlinx.serialization.modules.SerialNameCollisionInSealedClassesTest"
+
+class SerialNameCollisionInSealedClassesTest {
+    @Serializable
+    sealed class Base {
+        @Serializable
+        data class Child(val type: String, @SerialName("type2") val f: String = "2") : Base()
+    }
+
+    private fun Json(discriminator: String, useArrayPolymorphism: Boolean = false) = Json(
+        configuration = JsonConfiguration.Stable.copy(
+            classDiscriminator = discriminator,
+            useArrayPolymorphism = useArrayPolymorphism
+        )
+    )
+
+    @Test
+    fun testCollisionWithDiscriminator() {
+        assertFailsWith<IllegalStateException> { Json("type").stringify(Base.serializer(), Base.Child("a")) }
+        assertFailsWith<IllegalStateException> { Json("type2").stringify(Base.serializer(), Base.Child("a")) }
+        Json("f").stringify(Base.serializer(), Base.Child("a"))
+    }
+
+    @Test
+    fun testNoCollisionWithArrayPolymorphism() {
+        Json("type", true).stringify(Base.serializer(), Base.Child("a"))
+    }
+
+    @Serializable
+    sealed class BaseCollision {
+        @Serializable
+        class Child() : BaseCollision()
+
+        @Serializable
+        @SerialName("$prefix.BaseCollision.Child")
+        class ChildCollided() : BaseCollision()
+    }
+
+    @Test
+    fun testDescriptorInitializerFailure() {
+        BaseCollision.Child()
+        BaseCollision.ChildCollided()
+        BaseCollision.ChildCollided.serializer().descriptor // Doesn't fail
+        assertFailsWith<IllegalStateException> { BaseCollision.serializer().descriptor }
+    }
+}
