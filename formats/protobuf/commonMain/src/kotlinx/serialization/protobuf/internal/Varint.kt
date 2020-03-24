d79a8a5a709074c12f307aed9f5bf82ad9148e68
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2020-03-24 12:50:30 +0300

    Refactor ProtoBuf to be more maintenance-friendly, extract internal implementations to the corresponding package, rename internal classes to be consistent with their purpose

---
+++ formats/protobuf/commonMain/src/kotlinx/serialization/protobuf/internal/Varint.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2017-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package kotlinx.serialization.protobuf.internal
+
+import kotlinx.io.*
+import kotlin.jvm.*
+
+/**
+ *  Source for all varint operations:
+ *  https://github.com/addthis/stream-lib/blob/master/src/main/java/com/clearspring/analytics/util/Varint.java
+ */
+internal object Varint {
+    @JvmStatic
+    internal fun decodeSignedVarintInt(input: Input): Int {
+        val raw = input.readVarint32()
+        val temp = raw shl 31 shr 31 xor raw shr 1
+        // This extra step lets us deal with the largest signed values by treating
+        // negative results from read unsigned methods as like unsigned values.
+        // Must re-flip the top bit if the original read value had it set.
+        return temp xor (raw and (1 shl 31))
+    }
+
+    @JvmStatic
+    internal fun decodeSignedVarintLong(input: Input): Long {
+        val raw = input.readVarint64(false)
+        val temp = raw shl 63 shr 63 xor raw shr 1
+        // This extra step lets us deal with the largest signed values by treating
+        // negative results from read unsigned methods as like unsigned values
+        // Must re-flip the top bit if the original read value had it set.
+        return temp xor (raw and (1L shl 63))
+
+    }
+}
