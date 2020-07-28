370332bd7a763e9e1bdbd72d8f52fe524965099d
Leonid Startsev <sandwwraith@users.noreply.github.com>
2020-07-28 18:22:05 +0300

    [API stabilization] Properties unification (#938)
     Refactor Properties:
    
     - move to correct package
     - renamed load/store
     - remove *Nullable overloads
    
    * Do not write/read size of collection into Properties' map.
    
    Note that this introduces the same problem as with protobuf: we can't tell whether a map was empty or property with map type was missing, so maps should have `= emptyMap()` default value.
    
    Fixes #743
    
    Co-authored-by: Vsevolod Tolstopyatov <qwwdfsad@gmail.com>

--- formats/properties/commonTest/src/kotlinx/serialization/properties/PropertiesTest.kt
+++ formats/properties/commonTest/src/kotlinx/serialization/properties/PropertiesTest.kt
@@ -3,8 +3,10 @@
  */
 
 
-package kotlinx.serialization
+package kotlinx.serialization.properties
 
+import kotlinx.serialization.*
+import kotlinx.serialization.builtins.*
 import kotlin.test.*
 
 class PropertiesTest {
@@ -16,7 +18,7 @@ class PropertiesTest {
     data class Recursive(val data: Data, val property: String)
 
     @Serializable
-    data class NullableData(val nullable: String?, val nullable2: String?, val property: String)
+    data class NullableData(val property: String, val nullable: String? = null, val nullable2: String? = null)
 
     @Serializable
     data class Category(var name: String? = null, var subCategory: SubCategory? = null)
@@ -25,7 +27,7 @@ class PropertiesTest {
     data class SubCategory(var name: String? = null, var option: String? = null)
 
     @Serializable
-    data class DataWithMap(val map: Map<String, Int>)
+    data class DataWithMap(val map: Map<String, Int> = mapOf())
 
     @Serializable
     data class MultiType(
@@ -33,6 +35,12 @@ class PropertiesTest {
         val second: String,
         val unit: Unit = Unit,
         val last: Boolean = true
+    )
+
+    @Serializable
+    data class TestWithSize(
+        val p: String? = null,
+        val size: String? = null
     )
 
     @Serializable
@@ -48,9 +56,9 @@ class PropertiesTest {
         obj: T,
         serializer: KSerializer<T>
     ) {
-        val map = Properties.store(serializer, obj)
+        val map = Properties.encodeToMap(serializer, obj)
         assertEquals(expectedMap, map)
-        val unmap = Properties.load<T>(serializer, map)
+        val unmap = Properties.decodeFromMap<T>(serializer, map)
         assertEquals(obj, unmap)
     }
 
@@ -59,21 +67,25 @@ class PropertiesTest {
         obj: T,
         serializer: KSerializer<T>
     ) {
-        val map = Properties.storeNullable(serializer, obj)
+        val map = Properties.encodeToMap(serializer, obj)
         assertEquals(expectedMap, map)
-        val unmap = Properties.loadNullable<T>(serializer, map)
+        val unmap = Properties.decodeFromMap<T>(serializer, map)
         assertEquals(obj, unmap)
     }
 
     @Test
-    @Ignore // todo: unignore after migration to 1.3.70-eap-3
     fun testMultipleTypes() {
         val data = MultiType(1, "2")
         assertMappedAndRestored(
-            mapOf("first" to 1, "second" to "2", "unit" to Unit, "last" to true),
+            mapOf("first" to 1, "second" to "2", "last" to true),
             data,
             MultiType.serializer()
         )
+    }
+
+    @Test
+    fun testUnitIsEmptyMap() {
+        assertEquals(emptyMap(), Properties.encodeToMap(Unit.serializer(), Unit))
     }
 
     @Test
@@ -81,7 +93,6 @@ class PropertiesTest {
         val data = Data(listOf("element1"), "property")
         assertMappedAndRestored(
             mapOf(
-                "list.size" to 1,
                 "list.0" to "element1",
                 "property" to "property"
             ),
@@ -99,45 +110,35 @@ class PropertiesTest {
             ), "string"
         )
         val mapOf =
-            mapOf("data.list.size" to 1, "data.list.0" to "l1", "data.property" to "property", "property" to "string")
+            mapOf("data.list.0" to "l1", "data.property" to "property", "property" to "string")
         assertMappedAndRestored(mapOf, recursive, Recursive.serializer())
     }
 
     @Test
     fun testNullableProperties() {
-        val data = NullableData(null, null, "property")
-        val expectedMap = mapOf("nullable" to null, "nullable2" to null, "property" to "property")
+        val data = NullableData("property", null, null)
+        val expectedMap = mapOf("property" to "property")
         assertMappedNullableAndRestored(expectedMap, data, NullableData.serializer())
     }
 
     @Test
     fun testNestedNull() {
         val category = Category(name = "Name")
-        val expectedMap = mapOf("name" to "Name", "subCategory" to null)
-        assertMappedNullableAndRestored(expectedMap, category, Category.serializer())
-    }
-
-    @Test
-    fun testNestedNullable() {
-        val category = Category(
-            name = "Name",
-            subCategory = SubCategory()
-        )
-        val expectedMap = mapOf("name" to "Name", "subCategory.name" to null, "subCategory.option" to null)
+        val expectedMap = mapOf("name" to "Name")
         assertMappedNullableAndRestored(expectedMap, category, Category.serializer())
     }
 
     @Test
     fun testLoadOptionalProps() {
         val map: Map<String, Any> = mapOf("name" to "Name")
-        val restored = Properties.load<Category>(Category.serializer(), map)
+        val restored = Properties.decodeFromMap<Category>(Category.serializer(), map)
         assertEquals(Category("Name"), restored)
     }
 
     @Test
     fun testLoadOptionalNestedProps() {
         val map: Map<String, Any> = mapOf("name" to "Name", "subCategory.name" to "SubName")
-        val restored = Properties.load<Category>(Category.serializer(), map)
+        val restored = Properties.decodeFromMap<Category>(Category.serializer(), map)
         assertEquals(Category("Name", SubCategory("SubName")), restored)
     }
 
@@ -149,17 +150,10 @@ class PropertiesTest {
     }
 
     @Test
-    fun testLoadNullableOptionalNestedProps() {
-        val map: Map<String, Any?> = mapOf("name" to "Name", "subCategory.name" to null)
-        val restored = Properties.loadNullable<Category>(Category.serializer(), map)
-        assertEquals(Category("Name", SubCategory()), restored)
-    }
-
-    @Test
     fun testThrowsOnIncorrectMaps() {
-        val map: Map<String, Any?> = mapOf("name" to "Name")
+        val map: Map<String, Any> = mapOf("name" to "Name")
         assertFailsWith<SerializationException> {
-            Properties.loadNullable<Data>(Data.serializer(), map)
+            Properties.decodeFromMap(Data.serializer(), map)
         }
     }
 
@@ -170,11 +164,11 @@ class PropertiesTest {
         val map2 = DataWithMap(mapOf("one" to 1, "two" to 2))
 
         fun doTest(testData: DataWithMap) {
-            val map = Properties.store(
+            val map = Properties.encodeToMap(
                 DataWithMap.serializer(),
                 testData
             )
-            val d2 = Properties.load(
+            val d2 = Properties.decodeFromMap(
                 DataWithMap.serializer(),
                 map
             )
@@ -189,7 +183,7 @@ class PropertiesTest {
     @Test
     fun testEnumString() {
         val map = mapOf("data" to "ZERO")
-        val loaded = Properties.load(EnumData.serializer(), map)
+        val loaded = Properties.decodeFromMap(EnumData.serializer(), map)
         assertEquals(EnumData(TestEnum.ZERO), loaded)
     }
 
@@ -197,21 +191,12 @@ class PropertiesTest {
     @Test
     fun testEnumInteger() {
         val map = mapOf("data" to 0)
-        val loaded = Properties.load(EnumData.serializer(), map)
+        val loaded = Properties.decodeFromMap(EnumData.serializer(), map)
         assertEquals(EnumData(TestEnum.ZERO), loaded)
     }
 
     @Test
-    fun testNullableEnumString() {
-        val map = mapOf("data0" to null, "data1" to "ONE")
-        val loaded = Properties.loadNullable(NullableEnumData.serializer(), map)
-        assertEquals(NullableEnumData(null, TestEnum.ONE), loaded)
-    }
-
-    @Test
-    fun testNullableEnumInteger() {
-        val map = mapOf("data0" to null, "data1" to 1)
-        val loaded = Properties.loadNullable(NullableEnumData.serializer(), map)
-        assertEquals(NullableEnumData(null, TestEnum.ONE), loaded)
+    fun testCanReadSizeProperty() {
+        assertMappedAndRestored(mapOf("p" to "a", "size" to "b"), TestWithSize("a", "b"), TestWithSize.serializer())
     }
 }
