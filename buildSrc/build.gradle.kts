d73be3b6a3dce40ea6d297724c81296563302c52
anastasiia.spaseeva <anastasiia.spaseeva@jetbrains.com>
2020-10-28 15:13:09 +0300

    add space dev repo

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,20 +12,14 @@ plugins {
 repositories {
     mavenCentral()
     mavenLocal()
+    // kotlin-dev with space redirector
+    maven("https://cache-redirector.jetbrains.com/maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
 }
 
-val kotlinVersion = run {
-    if (project.hasProperty("build_snapshot_train")) {
-        val ver = project.properties["kotlin_snapshot_version"] as? String
-        require(!ver.isNullOrBlank()) {"kotlin_snapshot_version must be present if build_snapshot_train is used" }
-        return@run ver
-    }
-    val targetProp = if (project.hasProperty("bootstrap")) "kotlin.version.snapshot" else "kotlin.version"
-    FileInputStream(file("../gradle.properties")).use { propFile ->
-        val ver = Properties().apply { load(propFile) }[targetProp]
-        require(ver is String) { "$targetProp must be string in ../gradle.properties, got $ver instead" }
-        ver
-    }
+val kotlinVersion = FileInputStream(file("../gradle.properties")).use { propFile ->
+    val ver = Properties().apply { load(propFile) }["kotlin.version"]
+    require(ver is String) { "kotlin.version must be string in ../gradle.properties, got $ver instead" }
+    ver
 }
 
 dependencies {
