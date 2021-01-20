2d96a71299ec5f085ac21cfe149ca15cf32e6289
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2021-01-20 12:29:02 +0300

    Fail on primitive type overflow during JsonElement deserialization
    Fixes #1300

--- formats/json/commonTest/src/kotlinx/serialization/json/JsonParserFailureModesTest.kt
+++ formats/json/commonTest/src/kotlinx/serialization/json/JsonParserFailureModesTest.kt
@@ -54,6 +54,14 @@ class JsonParserFailureModesTest : JsonTestBase() {
                     it
                 )
             }
+            // 9223372036854775807 is Long.MAX_VALUE
+            assertFailsWith<JsonDecodingException> {
+                default.decodeFromString(
+                    Holder.serializer(),
+                    """{"id":${Long.MAX_VALUE}""" + "00" + "}",
+                    it
+                )
+            }
             assertFailsWith<JsonDecodingException> { default.decodeFromString(Holder.serializer(), """{"id"}""", it) }
             assertFailsWith<JsonDecodingException> { default.decodeFromString(Holder.serializer(), """{"id}""", it) }
             assertFailsWith<JsonDecodingException> { default.decodeFromString(Holder.serializer(), """{"i}""", it) }
@@ -70,7 +78,49 @@ class JsonParserFailureModesTest : JsonTestBase() {
 
     @Test
     fun testBoolean() = parametrizedTest {
-        assertFailsWith<JsonDecodingException> { default.decodeFromString(BooleanHolder.serializer(), """{"b": fals}""", it) }
-        assertFailsWith<JsonDecodingException> { default.decodeFromString(BooleanHolder.serializer(), """{"b": 123}""", it) }
+        assertFailsWith<JsonDecodingException> {
+            default.decodeFromString(
+                BooleanHolder.serializer(),
+                """{"b": fals}""",
+                it
+            )
+        }
+        assertFailsWith<JsonDecodingException> {
+            default.decodeFromString(
+                BooleanHolder.serializer(),
+                """{"b": 123}""",
+                it
+            )
+        }
+    }
+
+    @Serializable
+    class PrimitiveHolder(
+        val b: Byte = 0, val s: Short = 0, val i: Int = 0
+    )
+
+    @Test
+    fun testOverflow() = parametrizedTest {
+        // Byte overflow
+        assertFailsWith<JsonDecodingException> { default.decodeFromString<PrimitiveHolder>("""{"b": 128}""", it) }
+        // Short overflow
+        assertFailsWith<JsonDecodingException> { default.decodeFromString<PrimitiveHolder>("""{"s": 32768}""", it) }
+        // Int overflow
+        assertFailsWith<JsonDecodingException> {
+            default.decodeFromString<PrimitiveHolder>(
+                """{"i": 2147483648}""",
+                it
+            )
+        }
+    }
+
+    @Test
+    fun testNoOverflow() = parametrizedTest {
+        default.decodeFromString<PrimitiveHolder>("""{"b": ${Byte.MAX_VALUE}}""", it)
+        default.decodeFromString<PrimitiveHolder>("""{"b": ${Byte.MIN_VALUE}}""", it)
+        default.decodeFromString<PrimitiveHolder>("""{"s": ${Short.MAX_VALUE}}""", it)
+        default.decodeFromString<PrimitiveHolder>("""{"s": ${Short.MIN_VALUE}}""", it)
+        default.decodeFromString<PrimitiveHolder>("""{"i": ${Int.MAX_VALUE}}""", it)
+        default.decodeFromString<PrimitiveHolder>("""{"i": ${Int.MIN_VALUE}}""", it)
     }
 }
