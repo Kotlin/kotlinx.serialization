962d89e87b6b9bc362763c738ed64f7236c3a03b
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2021-02-26 13:14:02 +0300

    Add Jackson comparison with escape symbols

--- benchmark/src/jmh/kotlin/kotlinx/benchmarks/json/JacksonComparisonBenchmark.kt
+++ benchmark/src/jmh/kotlin/kotlinx/benchmarks/json/JacksonComparisonBenchmark.kt
@@ -48,6 +48,21 @@ open class JacksonComparisonBenchmark {
         cookies = "_ga=GA1.2.971852807.1546968515"
     )
 
+    private val dataWithEscapes = DefaultPixelEvent(
+        version = 1,
+        dateTime2 = System.currentTimeMillis().toString(),
+        serverName = "some-endp\"oint-qwer",
+        domain = "<a href=\"some.domain.com\">",
+        method = "POST",
+        clientIp = "127.0.0.1",
+        queryString = "anxa=CASCative&anxv=13.901.16.34566&anxe=\"FoolbarActive\"&anxt=E7AFBF15-1761-4343-92C1-78167ED19B1C&anxtv=13.901.16.34566&anxp=%5ECQ6%5Expt292%5ES33656%5Eus&anxsi&anxd=2019-10-08T17%3A03%3A57.246Z&f=00400000&anxr=1571945992297&coid=\"66abafd0d49f42e58dc7536109395306\"&userSegment&cwsid=opgkcnbminncdgghighmimmphiooeohh",
+        userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:70.0) Gecko/20100101 Firefox/70.0",
+        contentType = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        browserLanguage = "\"en\"-\"US\",en;\\q=0.5",
+        postData = "-",
+        cookies = "_ga=GA1.2.971852807.1546968515"
+    )
+
     private val stringData = Json.encodeToString(DefaultPixelEvent.serializer(), data)
 
     @Serializable
@@ -59,10 +74,16 @@ open class JacksonComparisonBenchmark {
     fun jacksonToString(): String = objectMapper.writeValueAsString(data)
 
     @Benchmark
+    fun jacksonToStringWithEscapes(): String = objectMapper.writeValueAsString(dataWithEscapes)
+
+    @Benchmark
     fun jacksonSmallToString(): String = objectMapper.writeValueAsString(smallData)
 
     @Benchmark
     fun kotlinToString(): String = Json.encodeToString(DefaultPixelEvent.serializer(), data)
+
+    @Benchmark
+    fun kotlinToStringWithEscapes(): String = Json.encodeToString(DefaultPixelEvent.serializer(), dataWithEscapes)
 
     @Benchmark
     fun kotlinSmallToString(): String = Json.encodeToString(SmallDataClass.serializer(), smallData)
@@ -72,4 +93,8 @@ open class JacksonComparisonBenchmark {
 
     @Benchmark
     fun kotlinFromString(): DefaultPixelEvent = Json.decodeFromString(DefaultPixelEvent.serializer(), stringData)
+}
+
+fun main() {
+    println(JacksonComparisonBenchmark().kotlinToString())
 }
