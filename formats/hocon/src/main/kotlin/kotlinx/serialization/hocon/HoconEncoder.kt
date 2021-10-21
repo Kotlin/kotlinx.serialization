bfa549a0863bd45289b800c965ecf715fee1bcda
Osip Fatkullin <osip.fatkullin@gmail.com>
2021-10-21 22:44:56 +0300

    Add enum encoding

--- formats/hocon/src/main/kotlin/kotlinx/serialization/hocon/HoconEncoder.kt
+++ formats/hocon/src/main/kotlin/kotlinx/serialization/hocon/HoconEncoder.kt
@@ -5,6 +5,7 @@ package kotlinx.serialization.hocon
 import com.typesafe.config.ConfigFactory
 import com.typesafe.config.ConfigValueFactory
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.descriptors.SerialDescriptor
 import kotlinx.serialization.internal.NamedValueEncoder
 import kotlinx.serialization.modules.SerializersModule
 
@@ -19,6 +20,10 @@ class HoconConfigEncoder(private val hocon: Hocon) : NamedValueEncoder() {
     override fun encodeTaggedValue(tag: String, value: Any) = withTaggedConfigValue(tag, value)
     override fun encodeTaggedNull(tag: String) = withTaggedConfigValue(tag, null)
     override fun encodeTaggedChar(tag: String, value: Char) = encodeTaggedString(tag, value.toString())
+
+    override fun encodeTaggedEnum(tag: String, enumDescriptor: SerialDescriptor, ordinal: Int) {
+        encodeTaggedString(tag, enumDescriptor.getElementName(ordinal))
+    }
 
     private fun withTaggedConfigValue(tag: String, value: Any?) {
         conf = conf.withValue(tag, ConfigValueFactory.fromAnyRef(value))
