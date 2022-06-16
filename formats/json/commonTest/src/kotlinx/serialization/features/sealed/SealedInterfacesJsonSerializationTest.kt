99dc54b02c124de5bfa9713b48f5341789599d05
Leonid Startsev <sandwwraith@gmail.com>
2022-06-16 15:33:30 +0200

    ~rebase fixup

--- formats/json/commonTest/src/kotlinx/serialization/features/sealed/SealedInterfacesJsonSerializationTest.kt
+++ formats/json/commonTest/src/kotlinx/serialization/features/sealed/SealedInterfacesJsonSerializationTest.kt
@@ -2,31 +2,34 @@
  * Copyright 2017-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
-package kotlinx.serialization.features
+package kotlinx.serialization.features.sealed
 
 import kotlinx.serialization.*
 import kotlinx.serialization.json.*
 import kotlin.test.*
 
-class SealedInterfacesJsonSerializationTest {
+class SealedInterfacesJsonSerializationTest : JsonTestBase() {
     @Serializable
     sealed interface I
 
     @Serializable
     sealed class Response: I {
         @Serializable
+        @SerialName("ResponseInt")
         data class ResponseInt(val i: Int): Response()
 
         @Serializable
+        @SerialName("ResponseString")
         data class ResponseString(val s: String): Response()
     }
 
     @Serializable
+    @SerialName("NoResponse")
     object NoResponse: I
 
     @Test
     fun testSealedInterfaceJson() {
-        val messages = listOf<I>(Response.ResponseInt(10), NoResponse, Response.ResponseString("foo"))
-        println(Json.encodeToString(messages))
+        val messages = listOf(Response.ResponseInt(10), NoResponse, Response.ResponseString("foo"))
+        assertJsonFormAndRestored(serializer(), messages, """[{"type":"ResponseInt","i":10},{"type":"NoResponse"},{"type":"ResponseString","s":"foo"}]""")
     }
 }
