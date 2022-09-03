765d46293d05ee20e733d7678dc6e42e0010b36b
Atsuto Yamashita <nyxht500@yahoo.co.jp>
2022-09-03 23:52:21 +0900

    fix typo (serialise -> serialize)

--- formats/json/jsMain/src/kotlinx/serialization/json/internal/DynamicEncoders.kt
+++ formats/json/jsMain/src/kotlinx/serialization/json/internal/DynamicEncoders.kt
@@ -257,7 +257,7 @@ private class DynamicPrimitiveEncoder(
         if (!json.configuration.isLenient && abs(value) > MAX_SAFE_INTEGER) {
             throw IllegalArgumentException(
                 "$value can't be deserialized to number due to a potential precision loss. " +
-                        "Use the JsonConfiguration option isLenient to serialise anyway"
+                        "Use the JsonConfiguration option isLenient to serialize anyway"
             )
         }
         encodeValue(asDouble)
