b74671e7e2369834571172fb7836033fe16c839f
Margarita Bobova <Margarita.Bobova@jetbrains.com>
2022-10-21 08:28:16 +0200

    Fix after KT-41642

--- formats/hocon/src/mainModule/kotlin/module-info.java
+++ formats/hocon/src/mainModule/kotlin/module-info.java
@@ -1,7 +1,6 @@
 module kotlinx.serialization.hocon {
     requires transitive kotlin.stdlib;
     requires transitive kotlinx.serialization.core;
-    requires transitive kotlin.stdlib.jdk8;
     requires transitive typesafe.config;
 
     exports kotlinx.serialization.hocon;
