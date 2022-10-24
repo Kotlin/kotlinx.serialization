87bbb2a7f0a374bf3c4f942c5b21759aa516e498
Alex Mihailov <av.mihailov.dev@gmail.com>
2022-10-24 15:11:59 +0300

    Add support for deserialization Duration in HOCON format

--- formats/hocon/src/mainModule/kotlin/module-info.java
+++ formats/hocon/src/mainModule/kotlin/module-info.java
@@ -1,6 +1,7 @@
 module kotlinx.serialization.hocon {
     requires transitive kotlin.stdlib;
     requires transitive kotlinx.serialization.core;
+    requires transitive kotlin.stdlib.jdk8;
     requires transitive typesafe.config;
 
     exports kotlinx.serialization.hocon;
