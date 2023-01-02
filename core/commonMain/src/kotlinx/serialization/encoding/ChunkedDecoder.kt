1581a129df3d218cda77f5cc092a8ababaefe759
alexey.sviridov <sviridov.alexey@gmail.com>
2023-01-02 01:04:31 +0100

    Fixed review defects - Added sample usage to method's KDoc

--- core/commonMain/src/kotlinx/serialization/encoding/ChunkedDecoder.kt
+++ core/commonMain/src/kotlinx/serialization/encoding/ChunkedDecoder.kt
@@ -11,6 +11,35 @@ public interface ChunkedDecoder {
      * Feeds string chunks to the provided consumer.
      *
      * @param consumeChunk - lambda function to handle strong chunks
+     *
+     * Example usage:
+     * ```
+     * @Serializable(with = LargeStringSerializer::class)
+     * data class LargeStringData(val largeString: String)
+     *
+     * @Serializable
+     * data class ClassWithLargeStringDataField(val largeStringField: LargeStringData)
+     *
+     * object LargeStringSerializer : KSerializer<LargeStringData> {
+     *     override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("LargeStringContent", PrimitiveKind.STRING)
+     *
+     *     override fun deserialize(decoder: Decoder): LargeStringData {
+     *         require(decoder is ChunkedDecoder) { "Only chunked decoder supported" }
+     *
+     *         val writer = FileWriter("/tmp/string.blob")
+     *
+     *         decoder.decodeStringChunked { chunk ->
+     *             writer.append(chunk)
+     *         }
+     *         writer.close()
+     *
+     *         return LargeStringData("file:///tmp/string.blob")
+     *     }
+     * }
+     * ```
+     *
+     * In this sample, we need to be able to handle huge string comes from json. Instead of storing it in memory,
+     * we're offload it into file and return file name instead
      */
     @ExperimentalSerializationApi
     public fun decodeStringChunked(consumeChunk: (chunk: String) -> Unit)
