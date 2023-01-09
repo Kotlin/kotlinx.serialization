a5d92ec747290199d7828883bd71b6b4d107e6ab
Sergey.Shanshin <sergey.shanshin@jetbrains.com>
2023-01-09 13:00:32 +0100

    ~remove comments

--- formats/json-tests/commonTest/src/kotlinx/serialization/SerializersLookupTest.kt
+++ formats/json-tests/commonTest/src/kotlinx/serialization/SerializersLookupTest.kt
@@ -279,13 +279,6 @@ class SerializersLookupTest : JsonTestBase() {
         }
     }
 
-// TODO uncomment when Kotlin 1.7.20 is released
-//    @Test
-//    fun testLookupDuration() = noLegacyJs {
-//        assertNotNull(serializerOrNull(typeOf<Duration>()))
-//        assertSame(Duration.serializer(), serializer<Duration>())
-//    }
-
     private inline fun <reified T> assertSerializedWithType(
         expected: String,
         value: T,
