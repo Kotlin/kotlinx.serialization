a20f4db6054d5d0d28ddecbef9c6600943ef6cbf
Leonid Startsev <sandwwraith@gmail.com>
2023-01-11 19:06:16 +0100

    Return KTypeWrapper

--- core/jvmMain/src/kotlinx/serialization/internal/Caching.kt
+++ core/jvmMain/src/kotlinx/serialization/internal/Caching.kt
@@ -106,46 +106,46 @@ private class CacheEntry<T>(@JvmField val serializer: KSerializer<T>?)
 /**
  * Workaround of https://youtrack.jetbrains.com/issue/KT-54611 and https://github.com/Kotlin/kotlinx.serialization/issues/2065
  */
-//private class KTypeWrapper(private val origin: KType) : KType {
-//    override val annotations: List<Annotation>
-//        get() = origin.annotations
-//    override val arguments: List<KTypeProjection>
-//        get() = origin.arguments
-//    override val classifier: KClassifier?
-//        get() = origin.classifier
-//    override val isMarkedNullable: Boolean
-//        get() = origin.isMarkedNullable
-//
-//    override fun equals(other: Any?): Boolean {
-//        if (other == null) return false
-//        if (origin != other) return false
-//
-//        val kClassifier = classifier
-//        if (kClassifier is KClass<*>) {
-//            val otherClassifier = (other as? KType)?.classifier
-//            if (otherClassifier == null || otherClassifier !is KClass<*>) {
-//                return false
-//            }
-//            return kClassifier.java == otherClassifier.java
-//        } else {
-//            return false
-//        }
-//    }
-//
-//    override fun hashCode(): Int {
-//        return origin.hashCode()
-//    }
-//
-//    override fun toString(): String {
-//        return "KTypeWrapper: $origin"
-//    }
-//}
+private class KTypeWrapper(private val origin: KType) : KType {
+    override val annotations: List<Annotation>
+        get() = origin.annotations
+    override val arguments: List<KTypeProjection>
+        get() = origin.arguments
+    override val classifier: KClassifier?
+        get() = origin.classifier
+    override val isMarkedNullable: Boolean
+        get() = origin.isMarkedNullable
+
+    override fun equals(other: Any?): Boolean {
+        if (other == null) return false
+        if (origin != other) return false
+
+        val kClassifier = classifier
+        if (kClassifier is KClass<*>) {
+            val otherClassifier = (other as? KType)?.classifier
+            if (otherClassifier == null || otherClassifier !is KClass<*>) {
+                return false
+            }
+            return kClassifier.java == otherClassifier.java
+        } else {
+            return false
+        }
+    }
+
+    override fun hashCode(): Int {
+        return origin.hashCode()
+    }
+
+    override fun toString(): String {
+        return "KTypeWrapper: $origin"
+    }
+}
 
 private class ParametrizedCacheEntry<T> {
     private val serializers: ConcurrentHashMap<List<KType>, Result<KSerializer<T>?>> = ConcurrentHashMap()
     inline fun computeIfAbsent(types: List<KType>, producer: () -> KSerializer<T>?): Result<KSerializer<T>?> {
-//        val wrappedTypes = types.map { KTypeWrapper(it) }
-        return serializers.getOrPut(types) {
+        val wrappedTypes = types.map { KTypeWrapper(it) }
+        return serializers.getOrPut(wrappedTypes) {
             kotlin.runCatching { producer() }
         }
     }
