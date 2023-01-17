8a22aee41be5d284775dcf1b5b6dea3add7ba67f
Sergey.Shanshin <sergey.shanshin@jetbrains.com>
2023-01-17 12:54:01 +0100

    ~remove suppress

--- core/commonMain/src/kotlinx/serialization/internal/NothingSerialDescriptor.kt
+++ core/commonMain/src/kotlinx/serialization/internal/NothingSerialDescriptor.kt
@@ -2,7 +2,6 @@
  * Copyright 2017-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
-@file:Suppress("FunctionName")
 @file:OptIn(ExperimentalSerializationApi::class)
 
 package kotlinx.serialization.internal
