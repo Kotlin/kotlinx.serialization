9afcfb61e677250b73ef0137a7dd5a603fae032f
Alexey Sviridov <sviridov.alexey@gmail.com>
2023-01-22 15:53:44 +0100

    Update core/commonMain/src/kotlinx/serialization/encoding/ChunkedDecoder.kt
    Co-authored-by: Leonid Startsev <sandwwraith@users.noreply.github.com>

--- core/commonMain/src/kotlinx/serialization/encoding/ChunkedDecoder.kt
+++ core/commonMain/src/kotlinx/serialization/encoding/ChunkedDecoder.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.ExperimentalSerializationApi
 @ExperimentalSerializationApi
 public interface ChunkedDecoder {
     /**
-     * Method allow decoding a string value by fixed-size chunks.
+     * Method allows decoding a string value by fixed-size chunks.
      * Usable for handling very large strings.
      * Chunk size defined in the JsonLexer#BATCH_SIZE constant.
      * Feeds string chunks to the provided consumer.
