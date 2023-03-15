051f1a212f9b08502c22f2c47ff9854e21d0ecd8
Leonid Startsev <sandwwraith@gmail.com>
2023-03-15 12:52:55 +0200

    K2 UP config: adapt build script to new TeamCity variables

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,6 +12,9 @@ plugins {
 repositories {
     mavenCentral()
     mavenLocal()
+    if (project.hasProperty("kotlin_repo_url")) {
+        maven(project.properties["kotlin_repo_url"] as String)
+    }
     // kotlin-dev with space redirector
     maven("https://cache-redirector.jetbrains.com/maven.pkg.jetbrains.space/kotlin/p/kotlin/dev")
 }
@@ -20,6 +23,11 @@ val kotlinVersion = run {
     if (project.hasProperty("build_snapshot_train")) {
         val ver = project.properties["kotlin_snapshot_version"] as? String
         require(!ver.isNullOrBlank()) {"kotlin_snapshot_version must be present if build_snapshot_train is used" }
+        return@run ver
+    }
+    if (project.hasProperty("kotlin_repo_url")) {
+        val ver = project.properties["kotlin_version"] as? String
+        require(!ver.isNullOrBlank()) {"kotlin_version must be present if kotlin_repo_url is used" }
         return@run ver
     }
     val targetProp = if (project.hasProperty("bootstrap")) "kotlin.version.snapshot" else "kotlin.version"
