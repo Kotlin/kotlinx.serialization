efae0dd8599d292532208a876c17ed691f617af9
Daniel Jasiński <daniel.jasinski@gmail.com>
2023-04-04 16:38:57 +0200

    Fix polymorphic serialization in Properties format

--- formats/properties/commonTest/src/kotlinx/serialization/properties/SealedClassSerializationFromPropertiesTest.kt
+++ formats/properties/commonTest/src/kotlinx/serialization/properties/SealedClassSerializationFromPropertiesTest.kt
@@ -32,19 +32,56 @@ class SealedClassSerializationFromPropertiesTest {
         val instance: BaseClass = Properties.decodeFromMap(props)
 
         assertIs<FirstChild>(instance)
-        assertEquals(instance.firstProperty, 1)
+        assertEquals(instance.firstProperty, 1L)
         assertEquals(instance.secondProperty, "one")
     }
 
     @Test
     fun testPropertiesSerialization() {
         val instance: BaseClass = FirstChild(
-            firstProperty = 1L, secondProperty = "one"
+            firstProperty = 1L,
+            secondProperty = "one"
         )
 
         val instanceProperties = Properties.encodeToMap(instance)
 
+        assertEquals("FIRSTCHILD", instanceProperties["type"])
         assertEquals(1L, instanceProperties["firstProperty"])
         assertEquals("one", instanceProperties["secondProperty"])
+    }
+
+    @Serializable
+    data class CompositeClass(val item: BaseClass)
+
+    @Test
+    fun testCompositeClassPropertiesDeserialization() {
+        val props = mapOf(
+            "item.type" to "SECONDCHILD",
+            "item.firstProperty" to 7L,
+            "item.secondProperty" to "nothing"
+        )
+
+        val composite: CompositeClass = Properties.decodeFromMap(props)
+        val instance = composite.item
+
+        assertIs<SecondChild>(instance)
+        assertEquals(instance.firstProperty, 7L)
+        assertEquals(instance.secondProperty, "nothing")
+    }
+
+    @Test
+    fun testCompositeClassPropertiesSerialization() {
+        val composite = CompositeClass(
+            item = FirstChild(
+                firstProperty = 5L,
+                secondProperty = "something"
+            )
+        )
+
+        val compositeProperties = Properties.encodeToMap(composite)
+
+        assertEquals("FIRSTCHILD", compositeProperties["item.type"])
+        assertEquals(5L, compositeProperties["item.firstProperty"])
+        assertEquals("something", compositeProperties["item.secondProperty"])
     }
 }
