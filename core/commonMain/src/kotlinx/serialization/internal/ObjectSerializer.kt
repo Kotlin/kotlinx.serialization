Kotlin/kotlinx.serialization

commit 42286566520a0dfac04e2d18a79330b6dd7a22dd

Iterate over element indicies in ObjectSerializer in order to let the format skip unknown keys (#1916)
Fixes #1915

core/commonMain/src/kotlinx/serialization/internal/ObjectSerializer.kt
@@ -40,7 +40,14 @@ internal class ObjectSerializer<T : Any>(serialName: String, private val objectI
     }
 
     override fun deserialize(decoder: Decoder): T {
-        decoder.beginStructure(descriptor).endStructure(descriptor)
+        decoder.decodeStructure(descriptor) {
+            when (val index = decodeElementIndex(descriptor)) {
+                CompositeDecoder.DECODE_DONE -> {
+                    return@decodeStructure
+                }
+                else -> throw SerializationException("Unexpected index $index")
+            }
+        }
         return objectInstance
     }
 }

formats/json/commonTest/src/kotlinx/serialization/features/ObjectSerialization.kt
@@ -5,10 +5,10 @@
 package kotlinx.serialization.features
 
 import kotlinx.serialization.*
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonTestBase
+import kotlinx.serialization.json.*
 import kotlinx.serialization.modules.*
-import kotlin.test.Test
+import kotlinx.serialization.test.*
+import kotlin.test.*
 
 class ObjectSerializationTest : JsonTestBase() {
 
@@ -45,5 +45,18 @@ class ObjectSerializationTest : JsonTestBase() {
             """{"response":{"type":"ApiResponse","message":"OK"}}""",
             json
         )
+    }
+
+    @Test
+    fun testUnknownKeys() {
+        val string = """{"metadata":"foo"}"""
+        assertFailsWithMessage<SerializationException>("ignoreUnknownKeys") {
+            Json.decodeFromString(
+                ApiResponse.Error.serializer(),
+                string
+            )
+        }
+        val json = Json { ignoreUnknownKeys = true }
+        assertEquals(ApiResponse.Error, json.decodeFromString(ApiResponse.Error.serializer(), string))
     }
 }

commit a3e6079b2555917b0e60a35faee6d333d2cf9076

ObjectSerializer: Respect sequential decoding
This was broken in https://github.com/Kotlin/kotlinx.serialization/pull/1916

core/commonMain/src/kotlinx/serialization/internal/ObjectSerializer.kt
@@ -41,6 +41,9 @@ internal class ObjectSerializer<T : Any>(serialName: String, private val objectI
 
     override fun deserialize(decoder: Decoder): T {
         decoder.decodeStructure(descriptor) {
+            if (decodeSequentially())
+                return@decodeStructure
+
             when (val index = decodeElementIndex(descriptor)) {
                 CompositeDecoder.DECODE_DONE -> {
                     return@decodeStructure