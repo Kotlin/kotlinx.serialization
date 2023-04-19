a68914484af2452e8b433389d5b6b371834bcd8f
Leonid Startsev <sandwwraith@gmail.com>
2023-04-19 17:29:55 +0200

    fixup! Fix incorrect json decoding iterator's .hasNext() behavior on array-wrapped inputs: hasNext() shouldn't throw exception if it was called more than once after the stream has ended.

--- formats/json/commonMain/src/kotlinx/serialization/json/internal/JsonIterator.kt
+++ formats/json/commonMain/src/kotlinx/serialization/json/internal/JsonIterator.kt
@@ -68,7 +68,7 @@ private class JsonIteratorArrayWrapped<T>(
     private val deserializer: DeserializationStrategy<T>
 ) : Iterator<T> {
     private var first = true
-    private var sawClosingBrace = false
+    private var finished = false
 
     override fun next(): T {
         if (first) {
@@ -84,8 +84,9 @@ private class JsonIteratorArrayWrapped<T>(
      * Note: if array separator (comma) is missing, hasNext() returns true, but next() throws an exception.
      */
     override fun hasNext(): Boolean {
+        if (finished) return false
         if (lexer.peekNextToken() == TC_END_LIST) {
-            sawClosingBrace = true
+            finished = true
             lexer.consumeNextToken(TC_END_LIST)
             if (lexer.isNotEof()) {
                 if (lexer.peekNextToken() == TC_BEGIN_LIST) lexer.fail("There is a start of the new array after the one parsed to sequence. " +
@@ -95,7 +96,7 @@ private class JsonIteratorArrayWrapped<T>(
             }
             return false
         }
-        if (!lexer.isNotEof() && !sawClosingBrace) lexer.fail(TC_END_LIST)
-        return !sawClosingBrace
+        if (!lexer.isNotEof() && !finished) lexer.fail(TC_END_LIST)
+        return true
     }
 }
