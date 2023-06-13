a4ecdfff5f68069ebed4c4e8db9479c6463b038b
Sergey.Shanshin <sergey.shanshin@jetbrains.com>
2023-06-13 14:58:48 +0200

    ~review fixes

--- formats/json/jvmMain/src/kotlinx/serialization/json/internal/CharsetReader.kt
+++ formats/json/jvmMain/src/kotlinx/serialization/json/internal/CharsetReader.kt
@@ -103,6 +103,7 @@ internal class CharsetReader(
             val remaining = if (position <= limit) limit - position else 0
             val bytesRead = inputStream.read(byteBuffer.array(), byteBuffer.arrayOffset() + position, remaining)
             if (bytesRead < 0) return bytesRead
+            // Method `position(I)LByteBuffer` does not exist in Java 8. For details, see comment for `flip` in `init` method
             (byteBuffer as Buffer).position(position + bytesRead)
         } finally {
             (byteBuffer as Buffer).flip() // see the `init` block in this class for the reasoning behind the cast
