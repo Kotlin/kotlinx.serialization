30a56adc94c25a702863f4e69bdc4d0d5851c425
Igor Yakovlev <igor.yakovlev@jetbrains.com>
2023-08-15 13:48:48 +0200

    DO WE NEED THIS? Native has same files either! Options: 1) Do the same with Native 2) Do not accept this commit 3) Accept this commit but not touch Native

--- formats/json/wasmMain/src/kotlinx/serialization/json/internal/CharArrayPool.kt
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2017-2022 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
- */
-package kotlinx.serialization.json.internal
-
-internal actual object CharArrayPoolBatchSize {
-    actual fun take(): CharArray = CharArray(BATCH_SIZE)
-    actual fun release(array: CharArray) = Unit
-}
