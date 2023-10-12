8c6cf7f93c5b0a815dd6725396fef5c3973eb4d6
Leonid Startsev <sandwwraith@gmail.com>
2023-10-12 11:16:03 +0200

    Build improvements: Kotlin 1.9.20-RC, default target hierarchy, bump Okio
    Updated to Kotlin 1.9.20-RC:
    - Kotlin 1.9.0/10 had some blockers in XCode 15 support.
    - Simplify WASM setup (it has been changed in 1.9.20).
    - Drop obsolete Native targets.
    
    Apply default target hierarchy:
    - Greatly simplified buildscript for native targets.
    - Supported targets are now runnable from gutter in the IDE.
    - Now all possible targets are built by default (without -Pnative.deploy flag), resulting in less confusion for contributors and CI (but longer local `check` times).
    - Publishing library with K2 should be possible now, no more incorrect `publishNativeKotlinMetadata` failing task.
    - More targets available in integration-test project.
    
    Updated Okio for:
    - linuxArm64 target support

--- formats/json-okio/build.gradle.kts
+++ formats/json-okio/build.gradle.kts
@@ -52,16 +52,11 @@ tasks.named<DokkaTaskPartial>("dokkaHtmlPartial") {
 }
 
 
-// Right now it is used for conditional support of kotlin 1.9.0 and 1.9.20+
 // TODO: Remove this after okio will be updated to the version with 1.9.20 stdlib dependency
-val kotlin_version: String by project
-val isNewWasmTargetEnabled = isKotlinVersionAtLeast(kotlin_version, 1, 9, 20)
-if (isNewWasmTargetEnabled) {
-    configurations.all {
-        resolutionStrategy.eachDependency {
-            if (requested.name == "kotlin-stdlib-wasm") {
-                useTarget("org.jetbrains.kotlin:kotlin-stdlib-wasm-js:${requested.version}")
-            }
+configurations.all {
+    resolutionStrategy.eachDependency {
+        if (requested.name == "kotlin-stdlib-wasm") {
+            useTarget("org.jetbrains.kotlin:kotlin-stdlib-wasm-js:${requested.version}")
         }
     }
 }
