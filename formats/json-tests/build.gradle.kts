d5a4d30d31e185fcfce1871ccfe7e1666fc97147
Leonid Startsev <sandwwraith@users.noreply.github.com>
2024-05-13 19:32:50 +0200

    Update okio to 3.9.0 version (#2671)

--- formats/json-tests/build.gradle.kts
+++ formats/json-tests/build.gradle.kts
@@ -2,7 +2,6 @@
  * Copyright 2017-2022 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 import Java9Modularity.configureJava9ModuleInfo
-import org.jetbrains.kotlin.gradle.targets.js.testing.*
 
 plugins {
     kotlin("multiplatform")
@@ -49,12 +48,3 @@ kotlin {
 }
 
 project.configureJava9ModuleInfo()
-
-// TODO: Remove this after okio will be updated to the version with 1.9.20 stdlib dependency
-configurations.all {
-    resolutionStrategy.eachDependency {
-        if (requested.name == "kotlin-stdlib-wasm") {
-            useTarget("org.jetbrains.kotlin:kotlin-stdlib-wasm-js:${requested.version}")
-        }
-    }
-}
