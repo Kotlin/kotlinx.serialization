ae90f82fee212a4da51f106ef14cc45140ebf1c9
Leonid Startsev <sandwwraith@users.noreply.github.com>
2024-05-14 14:07:57 +0200

    Apply suggestions from code review

--- formats/json/commonMain/src/kotlinx/serialization/json/JsonElement.kt
+++ formats/json/commonMain/src/kotlinx/serialization/json/JsonElement.kt
@@ -41,8 +41,8 @@ public sealed class JsonPrimitive : JsonElement() {
     public abstract val isString: Boolean
 
     /**
-     * Content of given element without quotes. For [JsonNull] this method returns `"null"`.
-     * [JsonPrimitive.contentOrNull] should be used for [JsonNull] to return `null`.
+     * Content of given element without quotes. For [JsonNull], this method returns a "null" string.
+     * [JsonPrimitive.contentOrNull] should be used for [JsonNull] to get a `null`.
      */
     public abstract val content: String
 
