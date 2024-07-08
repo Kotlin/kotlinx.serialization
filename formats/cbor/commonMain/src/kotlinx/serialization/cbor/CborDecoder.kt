c55ab97e12f75330261bb9eb689f70f78ea5daa5
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-07-08 19:53:40 +0200

    CBOR: fix missing API Docs

--- formats/cbor/commonMain/src/kotlinx/serialization/cbor/CborDecoder.kt
+++ formats/cbor/commonMain/src/kotlinx/serialization/cbor/CborDecoder.kt
@@ -6,7 +6,27 @@ package kotlinx.serialization.cbor
 
 import kotlinx.serialization.*
 
+/**
+ * This interface provides access to the current Cbor instance, so it can be properly taken into account in a
+ * custom serializer. For example, a custom serializer can decode CBOR data wrapped into a byte array using
+ * [Cbor.decodeFromByteArray] as required by some COSE structures.
+ * The actual CBOR Decoder used during deserialization implements this interface, so it is possible to cast the decoder
+ * passed to [KSerializer.deserialize] to [CborDecoder] when implementing such low-level serializers,
+ * to access configuration properties:
+ *
+ * ```kotlin
+ * override fun deserialize(decoder: Decoder): AlgorithmParameters {
+ *   if(decoder is CborDecoder){
+ *     val useDefiniteLengthEncoding = (decoder as CborDecoder).cbor.writeDefiniteLengths
+ *     //Do CBOR-agnostic low-level stuff
+ *   }
+ * }
+ * ```
+ */
 @ExperimentalSerializationApi
 public interface CborDecoder {
+    /**
+     * Exposes the current [Cbor] instance and all its configuration flags. Useful for low-level custom serializers.
+     */
     public val cbor: Cbor
 }
--- formats/cbor/commonMain/src/kotlinx/serialization/cbor/CborEncoder.kt
+++ formats/cbor/commonMain/src/kotlinx/serialization/cbor/CborEncoder.kt
@@ -6,7 +6,27 @@ package kotlinx.serialization.cbor
 
 import kotlinx.serialization.*
 
+/**
+ * This interface provides access to the current Cbor instance, so it can be properly taken into account in a
+ * custom serializer. For example, a custom serializer can output a byte array using [Cbor.encodeToByteArray]
+ * and embed resulting data into the output, as required, by some COSE structures.
+ * The actual CBOR Encoder used during serialization implements this interface, so it is possible to cast the encoder
+ * passed to [KSerializer.serialize] to [CborEncoder] when implementing such low-level serializers,
+ * to access configuration properties:
+ *
+ * ```kotlin
+ * override fun serialize(encoder: Encoder, value: AlgorithmParameters) {
+ *   if (encoder is CborEncoder) {
+ *     val useDefiniteLengthEncoding = (encoder as CborEncoder).cbor.writeDefiniteLengths
+ *     //Do CBOR-agnostic low-level stuff
+ *     }
+ * }
+ * ```
+ */
 @ExperimentalSerializationApi
 public interface CborEncoder {
+    /**
+     * Exposes the current [Cbor] instance and all its configuration flags. Useful for low-level custom serializers.
+     */
     public val cbor: Cbor
 }
