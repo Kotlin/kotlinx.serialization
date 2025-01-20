b2d86dce52c8733e2651c5dc7cc67f81e7f693ac
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2025-01-20 11:57:00 +0100

    Move generating Java code based on protobuf into a separate project
    Protobuf Gradle plugin expects that Gradle `java` plugin is present in the project, but KMP deprecated compatibility with `java` plugin. Soon this will become an error.
    
    To avoid, project error - generating Java code based on protobuf was moved into a separate Gradle subproject. 'kotlinx-serialization-protobuf' project Jvm test sources has now a normal dependency on this new subproject.

--- formats/protobuf/build.gradle.kts
+++ formats/protobuf/build.gradle.kts
@@ -2,29 +2,14 @@
  * Copyright 2017-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 import Java9Modularity.configureJava9ModuleInfo
-import com.google.protobuf.gradle.*
-import org.gradle.kotlin.dsl.protobuf
 
 plugins {
-    java // Needed for protobuf plugin only
     kotlin("multiplatform")
 
     alias(libs.plugins.serialization)
-    alias(libs.plugins.protobuf)
 
     id("native-targets-conventions")
     id("source-sets-conventions")
-}
-
-protobuf {
-    protobuf.protoc {
-        // Download from repositories
-        artifact = libs.protoc.get().toString()
-    }
-}
-
-tasks.clean {
-    delete(protobuf.protobuf.generatedFilesBaseDir)
 }
 
 kotlin {
@@ -40,24 +25,12 @@ kotlin {
         }
 
         jvmTest {
-            kotlin.srcDirs(file("${protobuf.protobuf.generatedFilesBaseDir}/test/java"))
-
             dependencies {
-                implementation(libs.protobuf.java)
+                implementation(project(":kotlinx-serialization-protobuf:proto-test-model"))
                 implementation(libs.kotlintest)
             }
         }
     }
-}
-
-sourceSets.test {
-    extensions.configure<SourceDirectorySet>("proto") {
-        srcDirs("testProto", "jvmTest/resources/common")
-    }
-}
-
-tasks.compileTestKotlinJvm {
-    dependsOn("generateTestProto")
 }
 
 configureJava9ModuleInfo()
