f2ef86d4a2d1861e4070d2038367ebbc721918a2
Margarita Bobova <margarita.bobova@jetbrains.com>
2025-02-05 14:08:32 +0100

    Remove usage of 'java' plugin
    Since 1.7.1 release, animalsniffer plugin is able to work with 'java-base' plugin which is applied and configured by KMP/Jvm target.

--- buildSrc/src/main/kotlin/animalsniffer-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/animalsniffer-conventions.gradle.kts
@@ -11,12 +11,7 @@ import ru.vyarus.gradle.plugin.animalsniffer.AnimalSnifferExtension
  */
 
 // Animalsniffer setup
-// Animalsniffer requires java plugin to be applied, but Kotlin 1.9.20
-// relies on `java-base` for Kotlin Multiplatforms `withJava` implementation
-// https://github.com/xvik/gradle-animalsniffer-plugin/issues/84
-// https://youtrack.jetbrains.com/issue/KT-59595
 plugins {
-    java
     id("ru.vyarus.animalsniffer")
 }
 
@@ -29,19 +24,6 @@ plugins.withId("org.jetbrains.kotlin.multiplatform") {
         configurations.findByName(outputConfigurationName)?.isCanBeConsumed = false
     }
 
-    disableJavaPluginTasks(extensions.getByName("sourceSets") as SourceSetContainer)
-}
-
-fun Project.disableJavaPluginTasks(javaSourceSet: SourceSetContainer) {
-    project.tasks.withType(Jar::class.java).named(javaSourceSet.getByName("main").jarTaskName).configure {
-        dependsOn("jvmTest")
-        enabled = false
-    }
-
-    project.tasks.withType(Test::class.java).named(JavaPlugin.TEST_TASK_NAME) {
-        dependsOn("jvmJar")
-        enabled = false
-    }
 }
 
 
