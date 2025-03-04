5b3515ec9334f697d00a4788f54e62ca531c753f
Dmitry Khalanskiy <Dmitry.Khalanskiy@jetbrains.com>
2025-03-04 17:00:01 +0100

    Move the tests for the Instant serializers
    Original version:
    https://github.com/Kotlin/kotlinx-datetime/blob/72681c2acaf9addf5effdef8ecd0975f6f7d10a7/serialization/common/test/InstantSerializationTest.kt

---
+++ formats/json-tests/commonTest/src/kotlinx/serialization/InstantSerializationTest.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2025-2025 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package kotlinx.serialization
+
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
+import kotlinx.serialization.builtins.*
+import kotlin.time.*
+import kotlin.test.*
+
+@OptIn(ExperimentalTime::class)
+class InstantSerializationTest {
+    private fun iso8601Serialization(serializer: KSerializer<Instant>) {
+        for ((instant, json) in listOf(
+            Pair(Instant.fromEpochSeconds(1607505416, 124000),
+                "\"2020-12-09T09:16:56.000124Z\""),
+            Pair(Instant.fromEpochSeconds(-1607505416, -124000),
+                "\"1919-01-23T14:43:03.999876Z\""),
+            Pair(Instant.fromEpochSeconds(987654321, 123456789),
+                "\"2001-04-19T04:25:21.123456789Z\""),
+            Pair(Instant.fromEpochSeconds(987654321, 0),
+                "\"2001-04-19T04:25:21Z\""),
+        )) {
+            assertEquals(json, Json.encodeToString(serializer, instant))
+            assertEquals(instant, Json.decodeFromString(serializer, json))
+        }
+    }
+
+    private fun componentSerialization(serializer: KSerializer<Instant>) {
+        for ((instant, json) in listOf(
+            Pair(Instant.fromEpochSeconds(1607505416, 124000),
+                "{\"epochSeconds\":1607505416,\"nanosecondsOfSecond\":124000}"),
+            Pair(Instant.fromEpochSeconds(-1607505416, -124000),
+                "{\"epochSeconds\":-1607505417,\"nanosecondsOfSecond\":999876000}"),
+            Pair(Instant.fromEpochSeconds(987654321, 123456789),
+                "{\"epochSeconds\":987654321,\"nanosecondsOfSecond\":123456789}"),
+            Pair(Instant.fromEpochSeconds(987654321, 0),
+                "{\"epochSeconds\":987654321}"),
+        )) {
+            assertEquals(json, Json.encodeToString(serializer, instant))
+            assertEquals(instant, Json.decodeFromString(serializer, json))
+        }
+        // check that having a `"nanosecondsOfSecond": 0` field doesn't break deserialization
+        assertEquals(Instant.fromEpochSeconds(987654321, 0),
+            Json.decodeFromString(serializer,
+                "{\"epochSeconds\":987654321,\"nanosecondsOfSecond\":0}"))
+        // "epochSeconds" should always be present
+        assertFailsWith<SerializationException> { Json.decodeFromString(serializer, "{}") }
+        assertFailsWith<SerializationException> { Json.decodeFromString(serializer, "{\"nanosecondsOfSecond\":3}") }
+    }
+
+    @Test
+    fun testIso8601Serialization() {
+        iso8601Serialization(Instant.serializer())
+    }
+
+    @Test
+    fun testComponentSerialization() {
+        componentSerialization(InstantComponentSerializer)
+    }
+
+    @Test
+    fun testDefaultSerializers() {
+        // should be the same as the ISO 8601
+        iso8601Serialization(Json.serializersModule.serializer())
+    }
+}
