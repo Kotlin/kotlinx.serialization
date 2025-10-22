8926b06af448b384a8b24e7a4fd658f40d1db2ef
Leonid Startsev <sandwwraith@gmail.com>
2025-10-22 16:01:56 +0500

    Add .serialName to MissingFieldException
    Make new constructor that requires it, and allow creation with nullable serial name only via internal mechanism.
    
    Old constructor without serial name should be eventually hidden.
    
    Fixes #2958

--- core/commonMain/src/kotlinx/serialization/internal/JsonInternalDependencies.kt
+++ core/commonMain/src/kotlinx/serialization/internal/JsonInternalDependencies.kt
@@ -1,14 +1,21 @@
 package kotlinx.serialization.internal
 
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.MissingFieldException
 import kotlinx.serialization.descriptors.*
 
 /*
  * Methods that are required for kotlinx-serialization-json, but are not effectively public.
  *
- * Anything marker with this annotation is not intended for public use.
+ * Anything marked with this annotation is not intended for public use.
  */
 @RequiresOptIn(level = RequiresOptIn.Level.ERROR)
 internal annotation class CoreFriendModuleApi
 
 @CoreFriendModuleApi
 public fun SerialDescriptor.jsonCachedSerialNames(): Set<String> = cachedSerialNames()
+
+@ExperimentalSerializationApi
+@CoreFriendModuleApi
+public fun missingFieldExceptionWithNewMessage(exception: MissingFieldException, message: String): MissingFieldException =
+    exception.withNewMessageInternal(message)
