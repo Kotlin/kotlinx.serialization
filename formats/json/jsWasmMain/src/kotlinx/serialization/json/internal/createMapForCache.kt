389f3948fa100a7f7268487f09d8aef3783fa245
Oleg Yukhnevich <whyoleg@gmail.com>
2026-01-22 17:44:46 +0200

    Remove doc comments from `actual`. `expect` is enough and it contains unresolved link

--- formats/json/jsWasmMain/src/kotlinx/serialization/json/internal/createMapForCache.kt
+++ formats/json/jsWasmMain/src/kotlinx/serialization/json/internal/createMapForCache.kt
@@ -4,9 +4,4 @@
 
 package kotlinx.serialization.json.internal
 
-/**
- * Creates a ConcurrentHashMap on JVM and regular HashMap on other platforms.
- * To make actual use of cache in Kotlin/Native, mark a top-level object with this map
- * as a @[ThreadLocal].
- */
 internal actual fun <K, V> createMapForCache(initialCapacity: Int): MutableMap<K, V> = HashMap(initialCapacity)
--- formats/json/jvmMain/src/kotlinx/serialization/json/internal/createMapForCache.kt
+++ formats/json/jvmMain/src/kotlinx/serialization/json/internal/createMapForCache.kt
@@ -6,10 +6,5 @@ package kotlinx.serialization.json.internal
 
 import java.util.concurrent.*
 
-/**
- * Creates a ConcurrentHashMap on JVM and regular HashMap on other platforms.
- * To make actual use of cache in Kotlin/Native, mark a top-level object with this map
- * as a @[ThreadLocal].
- */
 internal actual fun <K, V> createMapForCache(initialCapacity: Int): MutableMap<K, V> =
     ConcurrentHashMap(initialCapacity)
