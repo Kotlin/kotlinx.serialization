8a881dca6ee3c02da33fc6a0559017e03ed74544
Bernd Prünster <bernd.pruenster@a-sit.at>
2026-01-25 09:28:58 +0100

    Remove copyright headers from new CBOR files

--- formats/cbor/commonMain/src/kotlinx/serialization/cbor/CborElement.kt
+++ formats/cbor/commonMain/src/kotlinx/serialization/cbor/CborElement.kt
@@ -1,7 +1,3 @@
-/*
- * Copyright 2017-2025 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
- */
-
 @file:Suppress("unused")
 @file:OptIn(ExperimentalUnsignedTypes::class)
 
--- formats/cbor/commonTest/src/kotlinx/serialization/cbor/CborElementEqualityTest.kt
+++ formats/cbor/commonTest/src/kotlinx/serialization/cbor/CborElementEqualityTest.kt
@@ -1,7 +1,3 @@
-/*
- * Copyright 2017-2025 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
- */
-
 package kotlinx.serialization.cbor
 
 import kotlin.test.*
