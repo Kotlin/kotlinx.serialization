0cdff4b98ab4ef88d4cf6c4715ac9454bbdf116e
oungsi2000 <oungsi1000@gmail.com>
2026-01-30 14:34:32 +0900

    docs: add document for PolymorphicJsonDecoder

--- formats/json/commonMain/src/kotlinx/serialization/json/internal/PolymorphicJsonDecoder.kt
+++ formats/json/commonMain/src/kotlinx/serialization/json/internal/PolymorphicJsonDecoder.kt
@@ -6,6 +6,15 @@ package kotlinx.serialization.json.internal
 
 import kotlinx.serialization.json.JsonDecoder
 
+/**
+ * An internal extension of [JsonDecoder] that exposes the polymorphic discriminator.
+ * This interface is used during the transformation process to ensure that the
+ * class discriminator (e.g., the "type" field) is correctly propagated between
+ * decoders, especially when using [JsonTransformingSerializer].
+ */
 internal interface PolymorphicJsonDecoder : JsonDecoder {
+    /**
+     * A discriminator of the current [JsonDecoder].
+     */
     val discriminator:String?
 }
