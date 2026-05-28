0952eab27cbd412c9e6fbc1236884fccaf2b9802
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2026-05-28 14:04:36 -0400

    Setup cache redirector for the buildSrc

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -5,6 +5,40 @@ import java.util.*
  * Copyright 2017-2024 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 
+pluginManagement {
+    repositories {
+        /**
+         * Overrides for Teamcity 'K2 User Projects' + 'Aggregate build / Kotlinx libraries compilation' configuration:
+         * kotlin_repo_url - local repository with snapshot Kotlin compiler
+         * kotlin_version - kotlin version to use
+         * kotlin_language_version - LV to use
+         */
+        val kotlinRepoUrl: String? = providers.gradleProperty("kotlin_repo_url").orNull
+        if (kotlinRepoUrl?.isNotEmpty() == true) {
+            maven(kotlinRepoUrl)
+        }
+        /*
+        * This property group is used to build kotlinx.serialization against Kotlin compiler snapshot.
+        * When build_snapshot_train is set to true, kotlin_version property is used.
+        * DO NOT change the name of these properties without adapting kotlinx.train build chain.
+        */
+        val buildSnapshotTrain: String? = providers.gradleProperty("build_snapshot_train").orNull
+        if (buildSnapshotTrain.equals("true", true)) {
+            maven("https://oss.sonatype.org/content/repositories/snapshots")
+        }
+
+        // kotlin-dev with space redirector
+        maven("https://redirector.kotlinlang.org/maven/dev")
+        // Cache redirector for gradlePluginPortal
+        maven("https://cache-redirector.jetbrains.com/plugins.gradle.org/m2")
+        // gradlePluginPortal()
+        // Cache redirector for mavenCentral
+        maven("https://cache-redirector.jetbrains.com/repo.maven.apache.org/maven2")
+        // mavenCentral()
+        mavenLocal()
+    }
+}
+
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
