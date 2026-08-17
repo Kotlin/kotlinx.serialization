f1c9ef360d322605421288a9a1fa6e33449a5c8d
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2026-08-17 15:49:21 -0400

    Suppress LV 2.0 deprecation

--- buildSrc/src/main/kotlin/Java9Modularity.kt
+++ buildSrc/src/main/kotlin/Java9Modularity.kt
@@ -175,7 +175,7 @@ object Java9Modularity {
 
             val taskKotlinLanguageVersion = compilerOptions.languageVersion.orElse(KotlinVersion.DEFAULT)
             @OptIn(InternalKotlinGradlePluginApi::class)
-            @Suppress("DEPRECATION")
+            @Suppress("DEPRECATION", "DEPRECATION_ERROR")
             if (taskKotlinLanguageVersion.get() < KotlinVersion.KOTLIN_2_0) {
                 // part of work-around for https://youtrack.jetbrains.com/issue/KT-60541
                 @Suppress("INVISIBLE_MEMBER")
