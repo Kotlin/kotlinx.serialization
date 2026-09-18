a171b11d43c75bc0a6fdd556523def11093b218a
Oleg Yukhnevich <whyoleg@gmail.com>
2026-09-18 16:39:04 +0300

    Stabilize `kotlin.uuid.Uuid` serializer (#3263)
    `Uuid` class is stable starting from Kotlin 2.4.0
    
    Co-authored-by: Filipp Zhinkin <filipp.zhinkin@jetbrains.com>

--- core/jsMain/src/kotlinx/serialization/internal/Platform.kt
+++ core/jsMain/src/kotlinx/serialization/internal/Platform.kt
@@ -83,7 +83,7 @@ private val KClass<*>.isInterfaceHack: Boolean
         return js.asDynamic().`$metadata$`?.kind == "interface"
     }
 
-@OptIn(ExperimentalUnsignedTypes::class, ExperimentalUuidApi::class, ExperimentalSerializationApi::class)
+@OptIn(ExperimentalUnsignedTypes::class)
 internal actual fun initBuiltins(): Map<KClass<*>, KSerializer<*>> = mapOf(
     String::class to String.serializer(),
     Char::class to Char.serializer(),
